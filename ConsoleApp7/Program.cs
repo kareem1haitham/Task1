7b44a24feee574e5335b5d6eceb671f4b17ea26b
Mando <Mando@DESKTOP-I6N2CS5>
2025-07-28 12:48:12 +0300

    Add project files.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int perSmall = 25; 
+        int perLarge=35;
+        double taxRate = 0.06;
+        Console.WriteLine("Estimate for carpet cleaning service");
+        Console.Write("Number of small carpets: ");
+        int smallCarpets =Convert.ToInt32(Console.ReadLine());
+        Console.Write("Number of large carpets: ");
+        int largeCarpets =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Price per small room: {perSmall}$");
+        Console.WriteLine($"Price per large room: {perLarge}$");
+        int cost = (smallCarpets * perSmall) + (largeCarpets*perLarge);
+        Console.WriteLine($"Cost: {cost}$");
+        double taxi = cost * 0.06;
+        Console.WriteLine($"Tax: {taxi}");
+        Console.WriteLine("===============================");
+        Console.WriteLine($"Total estimate: {cost + taxi}");
+        Console.WriteLine("This estimate is valid for 30 days");
+
+    }
+}
---
+++ ConsoleApp7/Student.cs
@@ -0,0 +1,31 @@
+﻿internal class Student
+{
+    private string v1;
+    private string v2;
+    private object value;
+
+    public Student()
+    {
+    }
+
+    public Student(string v1, string v2)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+    }
+
+    public Student(string v1, string v2, object value)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.value = value;
+    }
+
+    public Student(string v1, string v2, object value, string v) : this(v1, v2, value)
+    {
+    }
+
+    public Student(string v1, string v2, object value, string v, string v3, string v4, string v5) : this(v1, v2, value, v)
+    {
+    }
+}
